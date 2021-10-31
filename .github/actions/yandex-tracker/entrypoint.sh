454ea3477246bdc0ae0232b90dc3b762908de7d1
Alexander Mamutov <alexander.mamutov@gmail.com>
2021-10-31 17:43:51 +0400

    Test

--- .github/actions/yandex-tracker/entrypoint.sh
+++ .github/actions/yandex-tracker/entrypoint.sh
@@ -1,39 +1,46 @@
 #!/bin/sh -l
 
-
 echo 'Check if task exists'
 
-existing=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/_search" \
+response=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/_search" \
    -H "Content-Type: application/json" \
    -H "Authorization: OAuth ${YANDEX_TRACKER_OAUTH}" \
    -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
-   -d "{\"filter\": {\"unique\": \"$TAG\"} }" \
-   | jq -r '.[0].key')
+   -d "{\"filter\": {\"unique\": \"$TAG\"} }")
 
-if test $existing = "null"; then
+echo $response
+
+existing=$(echo $response | jq -r '.[0].key')
+
+if test "$existing" = "null"; then
   echo 'Create new task'
-  existing=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/" \
-     -H "Content-Type: application/json" \
-     -H "Authorization: OAuth ${YANDEX_TRACKER_OAUTH}" \
-     -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
-     -d "{\"summary\": \"$TITLE\", \"queue\": \"$YANDEX_TRACKER_QUEUE\", \"description\": \"$DESCRIPTION\", \"unique\": \"$TAG\" }" \
-     | jq -r '.key')
+  response=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/" \
+       -H "Content-Type: application/json" \
+       -H "Authorization: OAuth ${YANDEX_TRACKER_OAUTH}" \
+       -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
+       -d "{\"summary\": \"$TITLE\", \"queue\": \"$YANDEX_TRACKER_QUEUE\", \"description\": \"$DESCRIPTION\", \"unique\": \"$TAG\" }")
+
+  echo $response
+
+  existing=$(echo $response | jq -r '.key')
 else
   echo 'Update task'
-  existing=$(curl -sS -X PATCH "https://api.tracker.yandex.net/v2/issues/$existing" \
+  response=$(curl -sS -X PATCH "https://api.tracker.yandex.net/v2/issues/$existing" \
     -H "Content-Type: application/json" \
     -H "Authorization: OAuth ${YANDEX_TRACKER_OAUTH}" \
     -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
-    -d "{\"summary\": \"$TITLE\", \"queue\": \"$YANDEX_TRACKER_QUEUE\", \"description\": \"$DESCRIPTION\", \"unique\": \"$TAG\" }" \
-    | jq -r '.key')
+    -d "{\"summary\": \"$TITLE\", \"queue\": \"$YANDEX_TRACKER_QUEUE\", \"description\": \"$DESCRIPTION\", \"unique\": \"$TAG\" }")
+
+  echo $response
 fi
 
 echo 'Add tests result'
-commentId=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/$existing/comments" \
+response=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/$existing/comments" \
      -H "Content-Type: application/json" \
      -H "Authorization: OAuth ${YANDEX_TRACKER_OAUTH}" \
      -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
-     -d "{\"text\": \"$TEST_RESULTS\" }" \
-     | jq -r '.id')
+     -d "{\"text\": \"$TEST_RESULTS\" }")
+
+echo $response
 
 echo 'Complete'
