d1d1095019d475fde8c1b431425db9b266863734
Alexander Mamutov <alexander.mamutov@gmail.com>
2021-10-31 20:57:40 +0400

    Test

--- .github/actions/yandex-tracker/entrypoint.sh
+++ .github/actions/yandex-tracker/entrypoint.sh
@@ -3,34 +3,27 @@
 
 echo 'Check if task exists'
 
-echo "!$YANDEX_TRACKER_OAUTH!"
-echo "!$YANDEX_TRACKER_ORG_ID!"
 
-curl -v -X POST https://api.tracker.yandex.net/v2/issues/_search \
-              -H "Content-Type: application/json" \
-              -H "Authorization: OAuth $YANDEX_TRACKER_OAUTH" \
-              -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
-              -d "{\"filter\": {\"unique\": \"123qwe\"} }"
 
-curl -v -X POST https://api.tracker.yandex.net/v2/issues/_search \
-   -H 'Content-Type: application/json' \
-   -H 'Authorization: OAuth AQAAAAADBdNFAAd47Vm91KDbOkMxhVSaFomrdCI' \
-   -H 'X-Org-Id: 6461097' \
-   -d '{"filter": {"unique": "123qwe"} }'
+response=$(curl -v -X POST https://api.tracker.yandex.net/v2/issues/_search \
+            -H "Content-Type: application/json" \
+            -H "Authorization: OAuth $YANDEX_TRACKER_OAUTH" \
+            -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
+            -d "{\"filter\": {\"unique\": \"$TAG\"} }")
 
 echo $response
 
-#existing=$(echo $response | jq -r '.[0].key')
-#
-#if test "$existing" = "null"; then
-#  echo 'Create new task'
-#  response=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/" \
-#       -H "Content-Type: application/json" \
-#       -H "Authorization: OAuth $YANDEX_TRACKER_OAUTH" \
-#       -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
-#       -d "{\"summary\": \"$TITLE\", \"queue\": \"$YANDEX_TRACKER_QUEUE\", \"description\": \"$DESCRIPTION\", \"unique\": \"$TAG\" }")
-#
-#  echo $response
+existing=$(echo $response | jq -r '.[0].key')
+
+if test "$existing" = "null"; then
+  echo 'Create new task'
+  response=$(curl -sS -X POST "https://api.tracker.yandex.net/v2/issues/" \
+       -H "Content-Type: application/json" \
+       -H "Authorization: OAuth $YANDEX_TRACKER_OAUTH" \
+       -H "X-Org-Id: $YANDEX_TRACKER_ORG_ID" \
+       -d "{\"summary\": \"$TITLE\", \"queue\": \"$YANDEX_TRACKER_QUEUE\", \"description\": \"$DESCRIPTION\", \"unique\": \"$TAG\" }")
+
+  echo $response
 #
 #  existing=$(echo $response | jq -r '.key')
 #else
