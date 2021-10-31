9dcb890624ae84a47f69fc1f9e79f69866d932ef
Alexander Mamutov <alexander.mamutov@gmail.com>
2021-10-31 19:27:16 +0400

    Test

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+const https = require('https')
+
+const options = {
+    port: 443,
+    hostname: 'api.tracker.yandex.net',
+    path: '/v2/issues/_search',
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "OAuth " + process.env.YANDEX_TRACKER_OAUTH,
+        "X-Org-Id": process.env.YANDEX_TRACKER_ORG_ID,
+    },
+    body: JSON.stringify({
+        filter: { unique: '123qwe' }
+    })
+}
+
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+        process.stdout.write(d)
+    })
+})
+
+req.on('error', error => {
+    console.error(error)
+})
+
+req.end()
